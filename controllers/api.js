ffd92689bf69a8bfcee77203392f68da3cbc2949
Uyi <ihazauyi@gmail.com>
2022-05-07 00:03:38 +0100

    feat: Validate the puzzle string on solve request

--- controllers/api.js
+++ controllers/api.js
@@ -5,6 +5,21 @@ const checkPlacement = (req, res) => {
   res.send("Check placement");
 };
 const solveSudoku = (req, res) => {
+  const puzzle = req.body.puzzle;
+  if (!puzzle) {
+    throw new Error('Required field missing');
+  }
+
+  const validity = solver.validate(puzzle);
+  if (!validity.validCharacters) {
+    throw new Error('Invalid characters in puzzle');
+  }
+  if (!validity.length) {
+    throw new Error('Expected puzzle to be 81 characters long');
+  }
+  if (!validity.solvable) {
+    throw new Error('Puzzle cannot be solved');
+  }
   res.send("Solve Sudoku");
 };
 
--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,16 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    const validity = {};
+    
+    const nonRequiredChars = /[^.0-9]/;
+    const valid = !nonRequiredChars.test(puzzleString);
+    validity.validCharacters = valid;
+
+    validity.length = puzzleString.length === 81 ? true : false;
+
+    validity.solvable = Math.random() > 0.5;
+    return validity;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
