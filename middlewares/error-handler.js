7117503e84a375eddc1a34abcce5ce4c0733d6d5
Uyi <ihazauyi@gmail.com>
2022-05-19 11:34:15 +0100

    fix(fcc): Change the default error message on unsolvable puzzle

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,7 +1,11 @@
 const errorHandler = (err, req, res, next) => {
   if (err instanceof Error) {
-    return res.status(400).json({error: err.message});
+    err.message =
+      err.message === "Puzzle could not be solved."
+        ? "Puzzle cannot be solved"
+        : err.message;
+    return res.status(400).json({ error: err.message });
   }
-  return res.status(500).json({error: err});
-}
+  return res.status(500).json({ error: err });
+};
 module.exports = errorHandler;
