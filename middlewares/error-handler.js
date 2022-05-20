3853bab13b10adb28cc647e51070de54e4aa5044
Uyi <ihazauyi@gmail.com>
2022-05-20 14:40:19 +0100

    style: Commenting/Rearrangement

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,11 +1,13 @@
 const errorHandler = (err, req, res, next) => {
   if (err instanceof Error) {
+    // Error thrown within the program
+    // The message provided by the library if it can't solve an array is changed
     err.message =
       err.message === "Puzzle could not be solved."
         ? "Puzzle cannot be solved"
         : err.message;
     return res.json({ error: err.message });
   }
-  return res.status(500).json({ error: err });
+  return res.status(500).json({ error: err }); // Some other error
 };
 module.exports = errorHandler;
