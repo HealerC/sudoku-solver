04f742783b662e2fa80570edef685ce4df29b839
Uyi <ihazauyi@gmail.com>
2022-05-20 14:20:23 +0100

    style: Rearrangement, commenting

--- controllers/api.js
+++ controllers/api.js
@@ -1,27 +1,25 @@
-const SudokuSolver = require("./sudoku-solver.js");
-const validate = require("./validator.js");
+const SudokuSolver = require("./sudoku-solver.js"); // Solves puzzles and checks placement
+const validate = require("./validator.js"); // Validate the arguments provided
 let solver = new SudokuSolver();
 
 const checkPlacement = (req, res) => {
   const { puzzle, coordinate, value } = req.body;
+
   if (!puzzle || !coordinate || !value) {
     throw new Error("Required field(s) missing");
   }
-  validate(puzzle, coordinate, value);
-  //const validity = solver.validate(puzzle, coordinate, value);
-  //solver.throwValidationErrors(validity);
+  validate(puzzle, coordinate, value); // Throws Error on invalid argument
+
   const placement = solver.checkPlacement(puzzle, coordinate, value);
   res.json(placement);
 };
 const solveSudoku = (req, res) => {
   const puzzle = req.body.puzzle;
+
   if (!puzzle) {
     throw new Error("Required field missing");
   }
-
   validate(puzzle);
-  //const validity = solver.validate(puzzle);
-  //solver.throwValidationErrors(validity);
 
   const puzzleSolution = solver.solve(puzzle);
   res.json({ solution: puzzleSolution });
